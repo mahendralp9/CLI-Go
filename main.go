27c4ca7ca9a54eb8ac6a09edcbc6a64a4559f0a5
mahendralp9 <mahendralantang@gmail.com>
2024-02-08 21:48:46 +0700

    write menu file in json

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 var menus = []string{}
 
-var filePath = "D:/"
+var filePath = "D:/GitWork/CLI-Golang"
 var path = filepath.Join(filePath, "CLI.json")
 
 func main() {
@@ -21,13 +22,59 @@ func main() {
 
 	var addMenus = append(menus, menu)
 
+	if err := writeFile(path, addMenus); err != nil {
+		fmt.Println("Error writing file : ", err)
+	}
+
+	//readfile menu
+	// menus := readFile(path)
+
+}
+
+func readFile(path string) ([]string, error) {
+
+	var menus []string
+
+	content, err := os.ReadFile(path)
+	if err != nil {
+
+		if os.IsNotExist(err) { //jika filenya tidak ketemu atau tidak ada maka akan menampilkan slice kosong
+			return menus, nil
+		}
+
+		json.Unmarshal(content, &menus)
+
+		return nil, err
+
+	}
+
+	return menus, nil
+}
+
+func writeFile(path string, addMenus []string) error {
+
+	type Menus struct {
+		Menu []string
+	}
+
+	menyu := Menus{
+		Menu: addMenus,
+	}
+
+	m, err := json.Marshal(menyu)
+	if err != nil {
+		return err
+	}
+
 	os.MkdirAll(filePath, os.ModePerm)
-	err := os.WriteFile(path, []byte(strings.Join(addMenus, "\n")), os.ModePerm)
+	err = os.WriteFile("CLI.json", m, os.ModePerm)
 	if err != nil {
 		log.Println("Error Creating File")
-		return
+		return nil
 	}
 
 	log.Println("File created successfully at: ", path)
+
+	return nil
 
 }
